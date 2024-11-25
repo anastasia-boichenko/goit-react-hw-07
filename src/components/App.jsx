ee50579d046032b07301b4cca02c710229a7ba5c
Anastasiia <anastasiaboyche85@gmail.com>
2024-11-25 01:40:36 +0100

    goit-react-hw-07

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
+
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsError, selectIsLoading } from "../redux/selectors";
+import { useEffect } from "react";
+import { fetchContactsThunk } from "../redux/contactsOps";
+import Loader from "./Loader/Loader";
+
+const App = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const isError = useSelector(selectIsError);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContactsThunk());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Phone-book</h1>
+      <div className="actionBox">
+        <ContactForm />
+        <SearchBox />
+      </div>
+      <div>
+        {isLoading && !isError && <Loader />}
+        {isError ? <p>Something went wrong</p> : <ContactList />}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,29 @@
+import { useId } from "react";
+import s from "./SearchBox.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { selectFilter } from "../../redux/selectors";
+import { changeFilter } from "../../redux/filterSlice";
+
+const SearchBox = () => {
+  const searchId = useId();
+  const dispatch = useDispatch();
+  const filterValue = useSelector(selectFilter);
+
+  return (
+    <div className={s.searchBox}>
+      <label htmlFor="searchId" className={s.searchLabel}>
+        Find contacts by name
+      </label>
+      <input
+        value={filterValue}
+        onChange={(e) => dispatch(changeFilter(e.target.value))}
+        type="text"
+        placeholder="Type your request..."
+        id={searchId}
+        className={s.searchField}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
